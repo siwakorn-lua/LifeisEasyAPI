0b905978fb921778b50676c9500bc3d9466ecc11
OOP <opkampitak@hotmail.com>
2019-04-30 14:41:23 +0700

    login api

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+require('dotenv').config()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/login', async (req,res) => {
+    const ticket = req.query.ticket
+    const result = await axios.get('https://account.it.chula.ac.th/serviceValidation',{
+        headers:{
+            DeeAppId:process.env.APP_ID,
+            DeeAppSecret:process.env.APP_SECRET,
+            DeeTicket:ticket
+        }
+    })
+    return res.json(result.data)
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
